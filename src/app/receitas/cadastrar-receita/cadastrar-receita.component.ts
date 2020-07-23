b217c07ba1632bc45dabde793d95b9d2bcc24d56
DeividsonGonini <deividsongonini@gmail.com>
2020-07-22 23:59:12 -0300

    Adicionado Detalhar Receita, corrigido Inserção de Categoria na receita e de Imagem quando nao colocam ao cadastrar

--- src/app/receitas/cadastrar-receita/cadastrar-receita.component.ts
+++ src/app/receitas/cadastrar-receita/cadastrar-receita.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReceitaService } from 'src/app/service/receita.service';
 import { Receita } from 'src/app/Model/Receita';
 import { Router } from '@angular/router';
+import { Categoria } from 'src/app/Model/Categoria';
 
 @Component({
   selector: 'app-cadastrar-receita',
@@ -13,17 +14,37 @@ export class CadastrarReceitaComponent implements OnInit {
   constructor(private receitaService: ReceitaService, private router: Router) { }
 
   receita: Receita = new Receita;
+  categoria: Categoria = new Categoria;
 
   ngOnInit(): void {
 
     window.scroll(0, 0);
   }
 
+  semImagem() {
+    // if (this.receita.img1 == null) {
+    //   this.receita.img1 = "https://i.imgur.com/VkD7vEA.png";
+    // }
+    if (this.receita.img2 == null) {
+      this.receita.img2 = "https://i.imgur.com/VkD7vEA.png";
+    }
+    if (this.receita.img3 == null) {
+      this.receita.img3 = "https://i.imgur.com/VkD7vEA.png";
+    }
+    console.log(this.receita.img2)
+    console.log(this.receita.img3)
+  }
+
+
   //Cadastrar Receita
   cadastrarReceita() {
+    this.semImagem();
+    this.categoria.id = parseInt((<HTMLSelectElement>document.getElementById("categoria")).value);
+    this.receita.idCategoria = this.categoria;
+    this.receita.ativo = true;
+
     this.receitaService.postReceita(this.receita).subscribe((resp: Receita) => {
       this.receita = resp;
-      this.receita.ativo = true;
       // this.receita.idUsuario.id = 1;
       this.router.navigate(['/receitas']);
     })
--- src/app/receitas/detalhar-receita/detalhar-receita.component.ts
+++ src/app/receitas/detalhar-receita/detalhar-receita.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Directive, HostListener } from '@angular/core';
+import { Receita } from 'src/app/Model/Receita';
+import { ReceitaService } from 'src/app/service/receita.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-detalhar-receita',
   templateUrl: './detalhar-receita.component.html',
   styleUrls: ['./detalhar-receita.component.css']
 })
+
 export class DetalharReceitaComponent implements OnInit {
 
-  constructor() { }
+  receita: Receita = new Receita;
+
+  constructor(private receitaService: ReceitaService, private route: ActivatedRoute, private location: Location) { }
+
+
+  // Botão Voltar
+  voltar() {
+    this.location.back();
+  }
 
   ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+
+    this.findById(id);
   }
+
+  findById(id: number) {
+    this.receitaService.getByIdReceita(id).subscribe((resp: Receita) => {
+      this.receita = resp;
+    })
+  }
+
+
 
 }
